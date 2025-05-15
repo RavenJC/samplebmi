57cc0cb568c848d61a21ab27f81f9c653973bf17
RavenJC <climacoravenjohn@gmail.com>
2025-05-15 10:32:24 +0800

    script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, update, remove } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCWTK3eJt1CuxZXjI30ZTO_AuDLygjhS08",
-  authDomain: "bmi2-3ccf9.firebaseapp.com",
-  projectId: "bmi2-3ccf9",
-  storageBucket: "bmi2-3ccf9.firebasestorage.app",
-  messagingSenderId: "144561292491",
-  appId: "1:144561292491:web:53183e298f7ba7f221074e"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyCWTK3eJt1CuxZXjI30ZTO_AuDLygjhS08",
+    authDomain: "bmi2-3ccf9.firebaseapp.com",
+    projectId: "bmi2-3ccf9",
+    storageBucket: "bmi2-3ccf9.firebasestorage.app",
+    messagingSenderId: "144561292491",
+    appId: "1:144561292491:web:53183e298f7ba7f221074e"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
